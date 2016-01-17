a70d3a5784383066f97c7b870c2262aaee2915de
absentees <scottblissett@gmail.com>
2016-01-17 18:03:07 +1100

    added pin library and variables

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+var  magnetSwitch;
+var magnet = require("pi-pins").connect(17);
+magnet.mode('in');
+magnetSwitch = 0;
+
 app.get('/', function (req, res) {
   res.send('Need2Wee online!')
 })
@@ -13,3 +18,7 @@ var server = app.listen(80, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
+magnet.on('both', function () {
+    console.log("magnet changed");
+});
