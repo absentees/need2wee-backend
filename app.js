b1edca61620e7d6fc659edf76767d85418c87c7e
absentees <scottblissett@gmail.com>
2016-01-21 10:09:42 +1100

    wee

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// Express stuff
 var express = require('express')
 var app = express()
+var firebase = require('firebase')
+var fb = new firebase('https://need2wee.firebaseio.com/');
 
+
+// Magnet stuff
 var  magnetSwitch;
 var magnet = require("pi-pins").connect(17);
 magnet.mode('in');
@@ -16,10 +21,17 @@ var server = app.listen(80, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port)
-  console.log(magnet.value());
 })
 
 magnet.on('both', function () {
-    console.log("magnet changed");
     console.log(magnet.value());
+		if (magnet.value() == true) {
+			fb.set({
+				occupied: "true"
+			});
+		} else if(magnet.value() == false){
+			fb.set({
+				occupied: "false"
+			});
+		}
 });
