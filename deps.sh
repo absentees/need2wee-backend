75d1dd5d546b6f863eff4fa475139199ef359480
absentees <scottblissett@gmail.com>
2016-11-04 11:24:49 +1100

    wee

--- deps.sh
+++ deps.sh
@@ -6,3 +6,7 @@ set -o pipefail
 apt-get update
 apt-get upgrade -y
 apt-get install -y libav-tools
+apt-get install -y alsa-utils libasound2-dev
+
+# Recent node now installs dependencies before running preinstall scripts :(
+npm install speaker@~0.2.6
