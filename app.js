3e3a52974a1b092258676cc788d34e1a9e347742
absentees <scottblissett@gmail.com>
2016-11-10 15:50:26 +1100

    wee

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var fb = new firebase('https://need2wee.firebaseio.com/occupied')
 var youtubeStream = require('youtube-audio-stream');
 var Lame = require('lame');
 var Speaker = require('speaker');
-var requestUrl = 'https://www.youtube.com/watch?v=rTyN-vvFIkE'; // Thinking music
 
 var toiletMusicStream;
 var toiletDecoder = new Lame.Decoder;
@@ -20,44 +19,53 @@ var magnet = require("pi-pins").connect(17);
 magnet.mode('in');
 magnetSwitch = 0;
 
-
+// Thinking music
+var clips = [
+  "rTyN-vvFIkE",
+  "Ag1o3koTLWM",
+  "hATmBOnMJJkE",
+  "kZ8KK8u9dN8"
+];
 
 app.get('/', function (req, res) {
   res.send('Need2Wee online: ' + magnet.value())
 });
 
 var server = app.listen(80, function () {
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Listening to wees on http://%s:%s', host, port)
 });
 
 magnet.on('both', function () {
 
-    console.log(magnet.value());
+console.log(magnet.value());
 
-		if (magnet.value() == true) {
-			fb.set({
-				occupied: "true"
-			});
+if (magnet.value() == true) {
+  fb.set({
+    occupied: "true"
+  });
 
-      try {
-        toiletMusicStream = youtubeStream(requestUrl).pipe(toiletDecoder).pipe(toiletSpeaker);
-      } catch (exception) {
-        console.log("Audio exception: " + exception);
-      }
+  try {
+    toiletMusicStream = youtubeStream(randomClip()).pipe(toiletDecoder).pipe(toiletSpeaker);
+  } catch (exception) {
+    console.log("Audio exception: " + exception);
+  }
 
-		} else if(magnet.value() == false){
-			fb.set({
-				occupied: "false"
-			});
+} else if(magnet.value() == false){
+  fb.set({
+    occupied: "false"
+  });
 
-      if (toiletMusicStream) {
-        toiletMusicStream.end();
-	toiletDecoder = new Lame.Decoder;
-	toiletSpeaker = new Speaker;
-      }
-		}
+  if (toiletMusicStream) {
+    toiletMusicStream.end();
+    toiletDecoder = new Lame.Decoder;
+    toiletSpeaker = new Speaker;
+  }
+}
 });
+
+function randomClip() {
+  var id = clips[Math.floor(Math.random()*clips.length)];
+  return 'https://www.youtube.com/watch?v=' + id;
+}
